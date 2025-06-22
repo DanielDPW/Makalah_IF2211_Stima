70e09a51de33cb79819b99061d4e006d4e65b70b
DanielDPW <150114531+DanielDPW@users.noreply.github.com>
2025-06-23 00:29:43 +0700

    feat: added some utility classes

---
+++ src/main/java/stimadpw/stima/algorithms/Constants.java
@@ -0,0 +1,30 @@
+package stimadpw.stima.algorithms;
+
+/**
+ * A utility class to hold all magic numbers and tunable constants for the pathfinder.
+ */
+public final class Constants {
+
+    private Constants() {}
+
+    // --- MOVEMENT PHYSICS & THRESHOLDS ---
+    public static final double DEFAULT_WALKING_FORCE = 0.3D;
+    public static final double IN_WATER_MOVEMENT_FORCE = 0.15D;
+    public static final double GO_SLOW_FORCE = 0.01D;
+    public static final double BLOCK_EDGE_UPPER_BOUND = 0.65;
+    public static final double BLOCK_EDGE_LOWER_BOUND = 0.35;
+
+    // --- JUMP COST CALCULATION CONSTANTS ---
+    public static final double JUMP_COST_DISTANCE_SCALAR = 1.3;
+    public static final double JUMP_COST_BENCHMARK_FORCE = 0.39D;
+
+    // --- JUMP FORCE CONSTANTS ---
+    public static final double FORCE_LONG_JUMP_A = 0.39D;
+    public static final double FORCE_LONG_JUMP_B = 0.35D;
+    public static final double FORCE_MEDIUM_JUMP_A = 0.32D;
+    public static final double FORCE_MEDIUM_JUMP_B = 0.30D;
+    public static final double FORCE_SHORT_JUMP_A = 0.25D;
+    public static final double FORCE_SHORT_JUMP_B = 0.18D;
+    public static final double FORCE_SHORT_JUMP_C = 0.15D;
+    public static final double FORCE_FALLING_MOVE = 0.1D;
+}
--- src/main/java/stimadpw/stima/algorithms/MovementUtils.java
+++ src/main/java/stimadpw/stima/algorithms/MovementUtils.java
@@ -1,4 +1,72 @@
 package stimadpw.stima.algorithms;
 
-public class MovementUtils {
+import stimadpw.stima.state.Node;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+
+/**
+ * A utility class containing static helper methods for movement calculations.
+ */
+public final class MovementUtils {
+    private MovementUtils() {}
+
+    /**
+     * Calculates the true 3D straight-line distance between two points.
+     * @param startPos The starting block position.
+     * @param endPos The ending block position.
+     * @return The direct-line distance in 3D space.
+     */
+    public static double getEuclideanDistance(BlockPos startPos, BlockPos endPos) {
+        return Math.sqrt(startPos.getSquaredDistance(endPos));
+    }
+
+    /**
+     * Calculates the cost of a single move from a parent node to a new position.
+     * This value is used to calculate the G-score of the new node.
+     * @param startNode The node we are moving from.
+     * @param destinationPos The block position we are moving to.
+     * @param isJump Is this move a jump?
+     * @param jumpStrength The force of the jump.
+     * @return The calculated cost for this single move.
+     */
+    public static double calculateMoveCost(Node startNode, BlockPos destinationPos, boolean isJump, double jumpStrength) {
+        BlockPos startPos = startNode.getPos();
+
+        // Calculate the 2D distance on the XZ plane for the move
+        double deltaX = startPos.getX() - destinationPos.getX();
+        double deltaZ = startPos.getZ() - destinationPos.getZ();
+        double horizontalDistance = Math.sqrt(deltaX * deltaX + deltaZ * deltaZ);
+
+        if (isJump) {
+            // Weaker/shorter jumps are more "expensive"
+            horizontalDistance = horizontalDistance / Constants.JUMP_COST_DISTANCE_SCALAR * Constants.JUMP_COST_BENCHMARK_FORCE / jumpStrength;
+        }
+
+        return horizontalDistance;
+    }
+
+    /**
+     * Checks if an entity's precise position is close enough to the center of a target block to be considered "arrived".
+     * @param targetBlock The destination block.
+     * @param currentEntityPos The entity's exact Vec3d position.
+     * @return true if the entity has arrived at the target block.
+     */
+    public static boolean hasArrived(BlockPos targetBlock, Vec3d currentEntityPos) {
+        double distanceSquared = Math.pow(targetBlock.getX() + 0.5D - currentEntityPos.getX(), 2) + Math.pow(targetBlock.getZ() + 0.5D - currentEntityPos.getZ(), 2);
+        return distanceSquared < 0.05;
+    }
+
+    /**
+     * Checks if an entity is near the edge of a block, used to time jumps correctly.
+     * @param entityPos The entity's exact Vec3d position.
+     * @return true if the entity is near the edge of the block they are in.
+     */
+    public static boolean isOnEdge(Vec3d entityPos) {
+        double xDecimal = Math.abs(entityPos.getX() % 1.0);
+        double zDecimal = Math.abs(entityPos.getZ() % 1.0);
+        return xDecimal > Constants.BLOCK_EDGE_UPPER_BOUND
+                || xDecimal < Constants.BLOCK_EDGE_LOWER_BOUND
+                || zDecimal > Constants.BLOCK_EDGE_UPPER_BOUND
+                || zDecimal < Constants.BLOCK_EDGE_LOWER_BOUND;
+    }
 }
