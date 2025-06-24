816526068596a86c7d72d6160d10813affe4595c
DanielDPW <150114531+DanielDPW@users.noreply.github.com>
2025-06-24 20:41:03 +0700

    chore: removed unused code

--- src/main/java/stimadpw/stima/state/BlockCache.java
+++ src/main/java/stimadpw/stima/state/BlockCache.java
@@ -8,44 +8,8 @@ public class BlockCache {
     private final int[] data = new int[Block.STATE_IDS.size()];
     private final BlockStateInterface bsi;
 
-    private static final int CACHED_MASK      = 1 << 0;
-    private static final int CAN_WALK_ON_MASK    = 1 << 1;
-    private static final int CAN_WALK_THROUGH_MASK = 1 << 2;
-
     public BlockCache(BlockStateInterface bsi) {
         this.bsi = bsi;
-    }
-
-    /**
-     * Computes the properties for a given BlockState and stores them in the cache.
-     */
-    private void computeBlockData(int id, BlockState state) {
-        int blockData = 0;
-
-        if (MovementHelper.canWalkOn(this.bsi, 0, 0, 0, state)) {
-            blockData |= CAN_WALK_ON_MASK;
-        }
-
-        if (MovementHelper.canWalkThrough(this.bsi, 0, 0, 0, state)) {
-            blockData |= CAN_WALK_THROUGH_MASK;
-        }
-
-        blockData |= CACHED_MASK; // Mark this block state as computed.
-        this.data[id] = blockData;
-    }
-
-    /**
-     * Retrieves the cached data for a BlockState, computing it if it doesn't exist yet.
-     */
-    private int getBlockData(BlockState state) {
-        int id = Block.getRawIdFromState(state);
-        int blockData = this.data[id];
-
-        if ((blockData & CACHED_MASK) == 0) {
-            computeBlockData(id, state);
-            blockData = this.data[id];
-        }
-        return blockData;
     }
 
     /**
